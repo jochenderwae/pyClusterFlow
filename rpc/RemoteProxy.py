450bbd4da2eb14206a4b82081eafd082e56c8f0e
Jochen Derwae <jochen.derwae@gmail.com>
2021-02-25 16:29:52 +0100

    Recreated RemoteProxy implementation

--- rpc/RemoteProxy.py
+++ rpc/RemoteProxy.py
@@ -3,61 +3,56 @@ import inspect
 
 is_worker = False
 
+def Remote(ctor):
+    if is_worker:
+        return ctor
+    return RemoteProxy(ctor)
+
+def Method(fn):
+    return fn
+
 class RemoteProxy(object):
-    @staticmethod
-    def SetWorker(_is_worker = True):
-        is_worker = _is_worker
 
-    @staticmethod
-    def IsWorker():
-        return is_worker
-
-    class Method(object):
-        def __init__(self, fn):
-            functools.update_wrapper(self, fn)
-            self.fn = fn
-            self.remote_proxy = None
-
-        def __call__(self, *args, **kwargs):
-            if(is_worker):
-                return self.fn
-            else:
-                return self.remote_proxy.do_remote(self.fn.__name__, *args, **kwargs)
-
-    def __init__(self, constructor):
-        self.constructor = constructor
-        self.remote_instance = None
-        self.exposed_methods = dict()
+    def __init__(self, ctor, *args, **kwargs):
+        self.ctor = ctor
+        self.args = args
+        self.kwargs = kwargs
 
     def __getattr__(self, method):
-        if method in self.exposed_methods:
-            return self.exposed_methods[method]
-        else:
-            raise AttributeError("{} is not a remotely accessible method in {}".format(method, self.constructor.__name__))
+        fn = self.ctor.__dict__[method]
+        if fn is None:
+            raise AttributeError("{} is not a remotely accessible method in {}".format(method, self.ctor.__name__))
+        print(fn.__name__)
+        print(fn)
+        def RemoteCaller(*args, **kwargs):
+            self.do_remote(method, *args, **kwargs)
+        return RemoteCaller
 
     def do_remote(self, method_name, *args, **kwargs):
-        fn = getattr(self.remote_instance, method_name).fn
-        if not fn is None:
-            fn(self.remote_instance, *args, **kwargs)
+        print("do remote call {}({},{})".format(method_name, args, kwargs))
 
     def __call__(self, *args, **kwargs):
-        # instead of making the instance locally, do this remotely
-        self.remote_instance = self.constructor(*args, **kwargs)
-
-        members = self.constructor.__dict__
-        for item in members:
-            if type(members[item]) == RemoteProxy.Method:
-                members[item].remote_proxy = self
-                self.exposed_methods[item] = members[item]
-
+        self.do_remote("constructor", *args, **kwargs)
         return self
+#    def __call__(self, *args, **kwargs):
+#        # instead of making the instance locally, do this remotely
+#        self.remote_instance = self.constructor(*args, **kwargs)
+
+
+#        members = self.constructor.__dict__
+#        for item in members:
+#            if type(members[item]) == RemoteProxy.Method:
+#                members[item].remote_proxy = self
+#                self.exposed_methods[item] = members[item]
+
+#        return self
 
 
 
-@RemoteProxy
+@Remote
 class TestClass(object):
 
-    @RemoteProxy.Method
+    @Method
     def remote_method(self):
         print("remote_method")
 
